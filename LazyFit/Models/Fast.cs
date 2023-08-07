7c51a7ad09a94ac293a8af3cdd4a08501720c26d
Patrik Šoma <soma.patrik@gmail.com>
2023-08-07 21:20:04 +0200

    Hours

--- LazyFit/Models/Fast.cs
+++ LazyFit/Models/Fast.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,25 @@ using System.Threading.Tasks;
 
 namespace LazyFit.Models
 {
-    internal class Fast
+    public class Fast
     {
-        DateTime StartTime;
-        DateTime EndTime;
+        [PrimaryKey]
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set;}
+        public int PlanMinutes { get; set; }
+        public bool Completed { get; set; }
+
+
+        public void SetHours(int hours)
+        {
+            PlanMinutes = hours * 60;
+        }
+
+        public void GetHoursMinutes(out int hours, out int minutes)
+        {
+            hours = PlanMinutes / 60;
+            minutes = PlanMinutes % 60;
+        }
     }
 }
---
+++ LazyFit/Services/DB.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyFit.Services
+{
+    public static class DB
+    {
+        #region DB settings
+
+        static SQLiteAsyncConnection Database;
+
+        public static string DatabaseFilename = "lazyfit.db3";
+
+        public static SQLite.SQLiteOpenFlags Flags =
+            // open the database in read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            // create the database if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            // enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+
+        public static async Task InitDB()
+        {
+            if (Database != null)
+                return;
+
+            Database = new SQLiteAsyncConnection(DatabasePath, Flags);
+        
+            //await Database.CreateTableAsync<PlcGroup>();
+            //await Database.CreateTableAsync<S7Plc>();
+            //await Database.CreateTableAsync<S7Address>();
+        }
+
+
+        #endregion
+
+    }
+}
