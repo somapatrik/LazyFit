09d733b16778b7e5e457a02273314b4c56001544
Patrik Šoma <soma.patrik@gmail.com>
2023-09-09 11:34:47 +0200

    lkjl

--- LazyFit/ViewModels/MainViewModel.cs
+++ LazyFit/ViewModels/MainViewModel.cs
@@ -21,34 +21,34 @@ namespace LazyFit.ViewModels
         public ICommand OpenResults => new Command(async () => await Shell.Current.Navigation.PushModalAsync(new ResultsPage()));
         public MainViewModel() 
         {
-            var entries = new[]
-{
-    new ChartEntry(212)
-    {
-        Label = "UWP",
-        ValueLabel = "112",
-        Color = SKColor.Parse("#2c3e50")
-    },
-    new ChartEntry(248)
-    {
-        Label = "Android",
-        ValueLabel = "648",
-        Color = SKColor.Parse("#77d065")
-    },
-    new ChartEntry(128)
-    {
-        Label = "iOS",
-        ValueLabel = "428",
-        Color = SKColor.Parse("#b455b6")
-    },
-    new ChartEntry(514)
-    {
-        Label = "Forms",
-        ValueLabel = "214",
-        Color = SKColor.Parse("#3498db")
-    }
-};
-            mychart = new BarChart { Entries = entries };
+//            var entries = new[]
+//{
+//    new ChartEntry(212)
+//    {
+//        Label = "UWP",
+//        ValueLabel = "112",
+//        Color = SKColor.Parse("#2c3e50")
+//    },
+//    new ChartEntry(248)
+//    {
+//        Label = "Android",
+//        ValueLabel = "648",
+//        Color = SKColor.Parse("#77d065")
+//    },
+//    new ChartEntry(128)
+//    {
+//        Label = "iOS",
+//        ValueLabel = "428",
+//        Color = SKColor.Parse("#b455b6")
+//    },
+//    new ChartEntry(514)
+//    {
+//        Label = "Forms",
+//        ValueLabel = "214",
+//        Color = SKColor.Parse("#3498db")
+//    }
+//};
+//            mychart = new BarChart { Entries = entries };
             LazyAsync();
         }
 
