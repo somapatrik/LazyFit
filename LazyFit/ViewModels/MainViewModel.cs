ce20521fb630738335981c85f2bd99af764fc596
Patrik Šoma <soma@hunterdouglas.cz>
2023-11-22 13:21:58 +0100

    x

--- LazyFit/ViewModels/MainViewModel.cs
+++ LazyFit/ViewModels/MainViewModel.cs
@@ -6,7 +6,7 @@ namespace LazyFit.ViewModels
 {
     public class MainViewModel : PrimeViewModel
     {
-        
+        public ICommand OpenX => new Command(async () => await Browser.Default.OpenAsync("https://www.x.com/lazyfitapp", BrowserLaunchMode.SystemPreferred));
         public ICommand OpenResults => new Command(async () => await Shell.Current.Navigation.PushModalAsync(new ResultsPage()));
 
         public string AppVersion  => AppInfo.VersionString;
