82ba84b3dbae43451f98b3295492ac8d483ecfc6
Patrik Šoma <soma@hunterdouglas.cz>
2023-11-27 14:51:52 +0100

    Blood pressure diagnosis

--- LazyFit/Models/Pressure/BloodPressure.cs
+++ LazyFit/Models/Pressure/BloodPressure.cs
@@ -1,9 +1,4 @@
 ﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LazyFit.Models.Pressure
 {
@@ -14,6 +9,8 @@ namespace LazyFit.Models.Pressure
         public int High { get; set; }
         public int Low { get; set; }
         public int Pulse { get; set; }
+
+        public PressureType Type { get; set; }
         
         public BloodPressure() { }
 
@@ -22,6 +19,49 @@ namespace LazyFit.Models.Pressure
             Id = id;
             High = high;
             Low = low;
+        }
+
+        private void Diagnose()
+        {
+            if ((High <= 90 ) || (Low <= 60))
+            {
+                Type = PressureType.Low;
+                return;
+            }
+
+            if ((High >= 180) || (Low >= 120))
+            {
+                Type = PressureType.Hypertensive;
+                return;
+            }
+
+
+
+            if ((High > 90 && High < 120) && (Low > 60 && Low <= 80))
+            {
+                Type = PressureType.Normal;
+                return;
+            }
+
+            if ((High >= 120 && High < 130) && (Low > 60 && Low <= 80))
+            {
+                Type = PressureType.Elevated;
+                return;
+            }
+
+            if ((High >= 130 && High < 140) || (Low > 80 && Low < 90))
+            {
+                Type = PressureType.High1;
+                return;
+            }
+
+            if ((High >= 140 && High < 180) || (Low >= 90 && Low < 120))
+            {
+                Type = PressureType.High2;
+                return;
+            }
+
+
         }
 
     }
---
+++ LazyFit/Models/Pressure/PressureType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyFit.Models.Pressure
+{
+    public enum PressureType
+    {
+        Low,
+        Normal,
+        Elevated,
+        High1, 
+        High2, 
+        Hypertensive
+    }
+}
