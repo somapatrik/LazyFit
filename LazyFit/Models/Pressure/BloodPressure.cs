4c38997935ca5ce037685f27a593b2a48d0a1088
Patrik Šoma <soma@hunterdouglas.cz>
2023-11-27 14:04:28 +0100

    Blood pressure model

---
+++ LazyFit/Models/Pressure/BloodPressure.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyFit.Models.Pressure
+{
+    public class BloodPressure
+    {
+        [PrimaryKey]
+        public Guid Id { get; set; }
+        public int High { get; set; }
+        public int Low { get; set; }
+        public int Pulse { get; set; }
+        
+        public BloodPressure() { }
+
+        public BloodPressure(Guid id,int high, int low)
+        {
+            Id = id;
+            High = high;
+            Low = low;
+        }
+
+    }
+}
