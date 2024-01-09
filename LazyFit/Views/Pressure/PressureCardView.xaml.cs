9ff795300665f8de1a04491a99948813076ab399
Patrik Šoma <soma@hunterdouglas.cz>
2024-01-09 15:22:55 +0100

    tak nic

--- LazyFit/Views/Pressure/PressureCardView.xaml.cs
+++ LazyFit/Views/Pressure/PressureCardView.xaml.cs
@@ -7,50 +7,18 @@ namespace LazyFit.Views.Pressure;
 
 public partial class PressureCardView : ContentView
 {
-   // PressureCardViewModel _viewModel;
 
     public static readonly BindableProperty BloodPressureProperty = BindableProperty.Create(nameof(BloodPressure), typeof(BloodPressure), typeof(PressureCardView));
 
     public BloodPressure BloodPressure
     {
         get => (BloodPressure)GetValue(BloodPressureProperty);
-        set
-        {
-            SetValue(BloodPressureProperty, value);
-            //SetViewModel();
-        }
+        set => SetValue(BloodPressureProperty, value);
     }
-
-    //private void SetViewModel()
-    //{
-    //    _viewModel = new PressureCardViewModel(BloodPressure);
-    //}
 
     public PressureCardView()
 	{
 		InitializeComponent();
-        //_viewModel = new PressureCardViewModel(BloodPressure);
-
-#if ANDROID
-
-		Microsoft.Maui.Handlers.BorderHandler.Mapper.AppendToMapping("longBorder", (handler, view) =>
-		{
-            handler.PlatformView.LongClick += PlatformView_LongClick;
-		});
-
-#endif
     }
 
-
-#if ANDROID
-    private async void PlatformView_LongClick(object sender, Android.Views.View.LongClickEventArgs e)
-    {
-        if (await Shell.Current.DisplayAlert("Remove pressure", "Remove", "Delete", "Cancel"))
-        {
-            //await _viewModel.DeletePressure();
-            await DB.DeleteItem(BloodPressure);
-            WeakReferenceMessenger.Default.Send(new Messages.RefreshPressureCards(true));
-        }
-    }
-#endif
 }
