1bb3e8f1ddaae825696f7b5328ee94d2319af134
Patrik Šoma <soma@hunterdouglas.cz>
2024-01-22 08:12:52 +0100

    view namespaces

--- LazyFit/ViewModels/Food/LogFoodViewModel.cs
+++ LazyFit/ViewModels/Food/LogFoodViewModel.cs
@@ -2,12 +2,8 @@
 using LazyFit.Models.Foods;
 using LazyFit.Services;
 using Mopups.Services;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Input;
 
 namespace LazyFit.ViewModels.Food
