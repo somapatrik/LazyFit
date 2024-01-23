7aafba55b02fbe7959d875622f8ae7eb36a32809
Patrik Šoma <soma@hunterdouglas.cz>
2024-01-23 10:20:46 +0100

    Pressure delete

--- LazyFit/Views/Pressure/PressureCardView.xaml.cs
+++ LazyFit/Views/Pressure/PressureCardView.xaml.cs
@@ -1,13 +1,11 @@
 using CommunityToolkit.Mvvm.Messaging;
 using LazyFit.Models.Pressure;
 using LazyFit.Services;
-using LazyFit.ViewModels.Pressure;
 
 namespace LazyFit.Views.Pressure;
 
 public partial class PressureCardView : ContentView
 {
-
     public static readonly BindableProperty BloodPressureProperty = BindableProperty.Create(nameof(BloodPressure), typeof(BloodPressure), typeof(PressureCardView));
 
     public BloodPressure BloodPressure
@@ -21,4 +19,13 @@ public partial class PressureCardView : ContentView
 		InitializeComponent();
     }
 
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        var bp = BloodPressure;
+        if (await Shell.Current.DisplayAlert($"Delete {bp.High} / {bp.Low}", $"Remove this entry from {bp.Time.ToString("d")} ?", "Delete", "Cancel"))
+        {
+            await DB.DeleteItem(bp);
+            WeakReferenceMessenger.Default.Send(new Messages.RefreshPressureCards(true));
+        }
+    }
 }
