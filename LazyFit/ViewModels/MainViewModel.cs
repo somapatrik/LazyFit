310b6a5a083bc0e15ec61ba44193af38ca0810f1
Patrik Šoma <soma.patrik@gmail.com>
2024-03-03 11:20:24 +0100

    Mainview ObservableObject

--- LazyFit/ViewModels/MainViewModel.cs
+++ LazyFit/ViewModels/MainViewModel.cs
@@ -1,23 +1,23 @@
-﻿using CommunityToolkit.Mvvm.Messaging;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LazyFit.Models.Pressure;
-using LazyFit.Services;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace LazyFit.ViewModels
 {
-    public class MainViewModel : PrimeViewModel
+    public partial class MainViewModel : ObservableObject // PrimeViewModel
     {
+        [ObservableProperty]
         private ObservableCollection<BloodPressure> _BloodPressures;
 
         public ICommand OpenX => new Command(async () => await Browser.Default.OpenAsync("https://www.x.com/lazyfitapp", BrowserLaunchMode.SystemPreferred));
         public ICommand OpenResults => new Command(async () => await Shell.Current.Navigation.PushModalAsync(new ResultsPage()));
 
-        public ObservableCollection<BloodPressure> BloodPressures 
-        {
-            get => _BloodPressures; 
-            set => SetProperty(ref _BloodPressures, value); 
-        }
+        //public ObservableCollection<BloodPressure> BloodPressures 
+        //{
+        //    get => _BloodPressures; 
+        //    set => SetProperty(ref _BloodPressures, value); 
+        //}
 
         public string AppVersion  => AppInfo.VersionString;
 
