f87fb1cdfe91888cfff685aabddedcd10cc225e6
Patrik Šoma <soma.patrik@gmail.com>
2024-09-16 22:36:30 +0200

    Nejde

---
+++ LazyFit/ViewModels/Mood/MoodChartViewModel.cs
@@ -0,0 +1,119 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LazyFit.Classes;
+using LazyFit.Services;
+using Microcharts;
+using SkiaSharp;
+
+
+namespace LazyFit.ViewModels.MoodViewModels
+{
+    internal partial class MoodChartViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private DateTime _FromDate;
+        partial void OnFromDateChanged(DateTime value)
+        {
+            LoadChart();
+        }
+
+        [ObservableProperty]
+        private DateTime _ToDate;
+        partial void OnToDateChanged(DateTime value)
+        {
+            LoadChart();
+        }
+
+        [ObservableProperty]
+        private Chart _ChartObject;
+
+        public MoodChartViewModel() 
+        {      
+            LoadChart();
+        }
+
+        private async void LoadChart()
+        {
+            var entries = GetEntriesReady();
+
+            // Get moods
+            var moods = await MoodService.GetMoods(FromDate, ToDate, true);
+
+            if (moods.Any())
+            {
+
+
+                // Group moods by day and Avg. value
+                var groupedMoods = moods.GroupBy(mood => mood.Time.Date)
+                    .Select(item => new DateFloat()
+                    {
+                        Date = item.Key,
+                        Value = item.Average(m => (float)m.TypeOfMood)
+                    })
+                    .ToList();
+
+                // Find same day as in entries
+                groupedMoods.ForEach(mood =>
+                {
+                    var found = entries.FirstOrDefault(ent => ent.Date == mood.Date);
+
+                    if (found != null)
+                        found.Value = mood.Value;
+                });
+
+            }
+
+            List<ChartEntry> chartEntries = new List<ChartEntry>();
+            entries.ForEach(entry =>
+            {
+                var color = SKColors.Transparent;
+
+                if (entry.Value >= 0 && entry.Value < 1)
+                    color = SKColors.Red;
+                else if (entry.Value >= 1 && entry.Value < 2)
+                    color = SKColors.IndianRed;
+                else if (entry.Value >= 2 && entry.Value < 3)
+                    color = SKColors.Orange;
+                else if (entry.Value >= 3 && entry.Value < 4)
+                    color = SKColors.LawnGreen;
+                else if (entry.Value >= 4)
+                    color = SKColors.LimeGreen;
+
+
+                float? EntryValue = entry.Value == -1 ? null : entry.Value;
+                chartEntries.Add(new ChartEntry(EntryValue)
+                {
+                    Label = entry.Date.ToString("d."),
+                    Color = color
+                });
+            });
+
+            ChartObject = new PointChart()
+            {
+                Entries = chartEntries,
+                MaxValue = 5,
+                MinValue = 0,
+                LabelTextSize = 32,
+                LabelOrientation = Orientation.Vertical,
+                PointSize = 25
+            };
+
+
+        }
+
+        private List<DateFloat> GetEntriesReady()
+        {
+            List<DateFloat> entries = new List<DateFloat>();
+
+            DateTime startDate = FromDate;
+            DateTime endDate = ToDate;
+
+            while (startDate.Date != endDate.Date)
+            {
+                entries.Add(new DateFloat() { Date = startDate, Value = -1 });
+                startDate = startDate.AddDays(1);
+            }
+
+            return entries;
+        }
+    }
+}
--- LazyFit/Views/Mood/MoodChartView.xaml.cs
+++ LazyFit/Views/Mood/MoodChartView.xaml.cs
@@ -1,9 +1,21 @@
+
+using LazyFit.ViewModels.MoodViewModels;
+using Microcharts;
+
 namespace LazyFit.Views.Mood;
 
 public partial class MoodChartView : ContentView
 {
-    public static readonly BindableProperty FromProperty = BindableProperty.Create(nameof(From), typeof(DateTime), typeof(MoodChartView));
-    public static readonly BindableProperty ToProperty = BindableProperty.Create(nameof(To), typeof(DateTime), typeof(MoodChartView));
+    public static readonly BindableProperty FromProperty = BindableProperty.Create(nameof(From), typeof(DateTime), typeof(MoodChartView), propertyChanged: OnFromToPropertyChanged);
+    public static readonly BindableProperty ToProperty = BindableProperty.Create(nameof(To), typeof(DateTime), typeof(MoodChartView), propertyChanged: OnFromToPropertyChanged);
+    public static readonly BindableProperty ChartProperty = BindableProperty.Create(nameof(Chart), typeof(Chart), typeof(MoodChartView), propertyChanged: OnFromToPropertyChanged);
+
+    public Chart Chart
+    {
+        get => (Chart)GetValue(FromProperty);
+        set => SetValue(FromProperty, value);
+    }
+
     public DateTime From
     {
         get => (DateTime)GetValue(FromProperty);
@@ -16,8 +28,22 @@ public partial class MoodChartView : ContentView
         set => SetValue(ToProperty, value);
     }
 
+    private static void OnFromToPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var view = (MoodChartView)bindable;
+        var viewModel = view.BindingContext as MoodChartViewModel;
+
+        if (viewModel != null)
+        {
+            viewModel.FromDate = view.From;
+            viewModel.ToDate = view.To;
+            view.Chart = viewModel.ChartObject;
+        }
+    }
+
     public MoodChartView()
 	{
 		InitializeComponent();
+        BindingContext = new MoodChartViewModel();
 	}
 }
